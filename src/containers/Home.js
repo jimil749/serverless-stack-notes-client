1d66cb110cf822121e9cbcce4f62b5e793c65fb4
Jimil Desai <jimildesai42@gmail.com>
2020-06-17 17:06:49 +0530

    Updating home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,6 +5,7 @@ import "./Home.css";
 import { API } from 'aws-amplify';
 import { onError } from '../libs/errorLib';
 import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
 
@@ -54,9 +55,17 @@ export default function Home() {
     function renderLander() {
         return (
             <div className="lander">
-                <h1> Scratch </h1>
-                <p>A simple note taking app</p>
+            <h1>Scratch</h1>
+            <p>A simple note taking app</p>
+            <div>
+              <Link to="/login" className="btn btn-info btn-lg">
+                Login
+              </Link>
+              <Link to="/signup" className="btn btn-success btn-lg">
+                Signup
+              </Link>
             </div>
+          </div>
         );
     }
 
